cc907be98a18b041d8be4099d3b098ed40613928
Christopher Bowers <c_bowers@me.com>
2022-02-05 18:18:41 -0500

    moved django folder and moved secret to .env

--- api/sencha_django/settings.py
+++ api/sencha_django/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$6$y6wgqmjidb^ca2ibkxqbz#ut9yeun3g$$1#jnvke!nl%zw#'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
