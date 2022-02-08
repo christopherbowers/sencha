1a9fb0a8187aca9d43bc1f8b917b67ded5b41efb
Christopher Bowers <c_bowers@me.com>
2022-02-08 17:30:23 -0500

    Fixed get all tickets to be admin only

--- api/tickets_api/views.py
+++ api/tickets_api/views.py
@@ -2,28 +2,25 @@ from rest_framework import generics
 from tickets.models import Ticket
 from .serializers import TicketSerializer
 from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAdminUser, IsAuthenticated
-# IsADjangoModelPermissionsOrAnonReadOnly
 
 
-class TicketUserWritePermission(BasePermission):
+class TicketUserPermission(BasePermission):
   message = 'Editing posts is restricted to the creator only.'
 
   def has_object_permission(self, request, view, obj):
 
-    # if request.method in SAFE_METHODS:
     if request.user != obj.created_by:
       return False
-
     else:
       return obj.created_by == request.user
 
 
-class TicketList(generics.ListCreateAPIView, TicketUserWritePermission):
-  permission_classes = [TicketUserWritePermission]
-  queryset = Ticket.ticketobjects.all()
+class TicketList(generics.ListCreateAPIView, TicketUserPermission):
+  permission_classes = [IsAdminUser]
+  queryset = Ticket.objects.all()
   serializer_class = TicketSerializer
 
-class TicketDetails(generics.RetrieveUpdateDestroyAPIView, TicketUserWritePermission):
-  permission_classes = [TicketUserWritePermission]
+class TicketDetails(generics.RetrieveUpdateDestroyAPIView, TicketUserPermission):
+  permission_classes = [TicketUserPermission]
   queryset = Ticket.objects.all()
   serializer_class = TicketSerializer
