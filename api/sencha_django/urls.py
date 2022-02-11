599343a98fc6431eb2918d2182955de1820324bc
Christopher Bowers <c_bowers@me.com>
2022-02-11 09:39:43 -0500

    added jdoser

--- api/sencha_django/urls.py
+++ api/sencha_django/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
   path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
   path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-  path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
   path('admin/', admin.site.urls),
   path('', include('tickets.urls', namespace='tickets')),
   path('api/tickets/', include('tickets_api.urls', namespace='tickets_api')),
   path('api/users/', include('users.urls')),
+  path('api/', include('djoser.urls')),
 ]
